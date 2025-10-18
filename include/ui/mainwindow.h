a0266c58035479ba517a86c1c32c0a973b133a90
Artem <sm1kyx@gmail.com>
2025-10-18 13:36:03 +0200

    Добавить переопределение метода dragEnterEvent в классе MainWindow и исправить порядок подключения заголовка mainwindow_interface.h в GroupUpdater

--- include/ui/mainwindow.h
+++ include/ui/mainwindow.h
@@ -236,7 +236,7 @@ private:
 
     void closeEvent(QCloseEvent *event) override;
 
-    void dragEnterEvent(QDragEnterEvent *event);
+    void dragEnterEvent(QDragEnterEvent *event) override;
 
     void dropEvent(QDropEvent* event) override;
 
