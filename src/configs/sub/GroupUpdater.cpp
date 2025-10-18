684e89703982fe897b96ac77b9cb42c527947b36
Artem <sm1kyx@gmail.com>
2025-10-18 14:10:26 +0200

    Удалить закомментированный код для обработки дубликатов подписок и улучшить логику отображения меню обновлений в MainWindow

--- src/configs/sub/GroupUpdater.cpp
+++ src/configs/sub/GroupUpdater.cpp
@@ -958,10 +958,10 @@ namespace Subscription {
                     toDelete << id;
                 }
 
-                runOnUiThread([=] {
-                    MessageBoxWarning("Duplicate Subscription Groups Removed",
-                        QString("Removed %1 duplicate subscription groups").arg(toDelete.size()));
-                });
+                // runOnUiThread([=] {
+                //     MessageBoxWarning("Duplicate Subscription Groups Removed",
+                //         QString("Removed %1 duplicate subscription groups").arg(toDelete.size()));
+                // });
 
                 // Удаляем найденные дубликаты
                 for (int id : toDelete) {
