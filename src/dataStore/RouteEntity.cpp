98b93b17b7fc3dbca80159e22a00fa5c8e8e0310
Artem <sm1kyx@gmail.com>
2025-10-28 20:00:40 +0100

    fix: Упростить разбор строки в функции add_simple_process_rule

--- src/dataStore/RouteEntity.cpp
+++ src/dataStore/RouteEntity.cpp
@@ -784,19 +784,25 @@ namespace Configs {
 
     bool RoutingChain::add_simple_process_rule(const QString& content, const std::shared_ptr<RouteRule>& rule, ruleType type)
     {
-        auto sp = content.split(":");
-        if (sp.size() != 2) return false;
-        const QString& address = sp[1];
-        const QString& subType = sp[0];
+        int colonIdx = content.indexOf(':');
+        if (colonIdx == -1) return false;
+
+        QString subType = content.left(colonIdx).trimmed();
+        QString address = content.mid(colonIdx + 1).trimmed();
+
         if (subType == "processName" && type == simpleProcessName)
         {
-            if (!rule->process_name.contains(address)) rule->process_name.append(address);
+            if (!rule->process_name.contains(address))
+                rule->process_name.append(address);
             return true;
-        } else if (subType == "processPath" && type == simpleProcessPath)
+        }
+        else if (subType == "processPath" && type == simpleProcessPath)
         {
-            if (!rule->process_path.contains(address)) rule->process_path.append(address);
+            if (!rule->process_path.contains(address))
+                rule->process_path.append(address);
             return true;
-        } else
+        }
+        else
         {
             return false;
         }
